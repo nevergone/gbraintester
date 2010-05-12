735763ff7bffc4b67fbe9d4a0769dfb04459ba7b
Kurucz István <kurucz.istvan@gmail.com>
2010-05-12 21:44:45 +0200

    non-exist directory is not verified in plugin_list_filename() function

--- src/pluginloader.c
+++ src/pluginloader.c
@@ -26,17 +26,11 @@
 
 /* reading "path" directory, and results list with plugin filename */
 GList * plugin_list_filename (const gchar *path) {
-	GError *error = NULL;
 	GDir *dir;
 	const gchar *filename;
 	TestPlugin *PluginEntity;
 
-	dir = g_dir_open(path, 0, &error);
-	if (!dir) { /* if not success */
-		g_warning("%s", error->message);
-		g_error_free(error);
-		return NULL;
-	}
+	dir = g_dir_open(path, 0, NULL);
 	while ((filename = g_dir_read_name(dir))) { /* get file name in directory */
 		if (!g_str_has_suffix(filename, G_MODULE_SUFFIX)) /* if not module */
 			continue;
